c11371f30ba8f5227c55ec83c024ff8a45dd99f3
Mika Ipstenu Epstein <ipstenu@ipstenu.org>
2017-03-23 12:42:39 -0700

    Adding shortcode for author-boxes

--- shortcodes.php
+++ shortcodes.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Global Shortcodes
 Description: Various shortcodes used on the LeZWatch Network
-Version: 1.1
+Version: 1.2
 Author: Mika Epstein
 */
 
@@ -22,6 +22,7 @@ class LezWatch_Shortcodes{
 		add_shortcode( 'copyright', array( $this, 'copyright') );
 		add_shortcode( 'numposts', array( $this, 'numposts') );
 		add_shortcode( 'google-ads', array( $this, 'google_ads') );
+		add_shortcode( 'author-box', array( $this, 'author_box' ) );
 	}
 
 	/*
@@ -34,22 +35,22 @@ class LezWatch_Shortcodes{
 	 *		text = (text) copyright message. (default: &copy; )
 	 *
 	 * @since 1.0
-	 */	
+	 */
 	public function copyright( $atts ) {
 	    $attributes = shortcode_atts( array(
 	        'year' => 'auto',
 	        'text' => '&copy;'
 	    ), $atts );
-	    
+
 		$year = ( $attributes['year'] == '' || ctype_digit($attributes['year']) == false )? date('Y') : intval($attributes['year']);
 		$text = ( $attributes['text'] == '' )? '&copy;' : sanitize_text_field( $attributes['text'] );
-	
+
 		if ( $year == date('Y') || $year > date('Y') ) {
 			$output = date('Y');
 		} elseif ( $year < date('Y') ) {
 			$output = $year . ' - ' . date('Y');
 		}
-	
+
 		return $text . ' ' . $output;
 	}
 
@@ -78,9 +79,9 @@ class LezWatch_Shortcodes{
 
 			// Collect posts
 			$posttype = sanitize_text_field( $attr['posttype'] );
-			
+
 			if ( post_type_exists( $posttype ) !== true ) $posttype = 'post';
-			
+
 			$to_count = wp_count_posts( $posttype );
 			$return = $to_count->publish;
 
@@ -90,7 +91,7 @@ class LezWatch_Shortcodes{
 			$the_term     = sanitize_text_field( $attr['term'] );
 			$the_taxonomy = sanitize_text_field( $attr['taxonomy'] );
 
-			if ( !is_null($the_term) && $the_taxonomy !== false ) {			
+			if ( !is_null($the_term) && $the_taxonomy !== false ) {
 				$all_taxonomies = ( empty( $the_taxonomy ) )? get_taxonomies() : array( $the_taxonomy ) ;
 
 				foreach ( $all_taxonomies as $taxonomy ) {
@@ -110,7 +111,7 @@ class LezWatch_Shortcodes{
 		} else {
 			$return = 'n/a';
 		}
-		
+
 		return $return;
 	}
 
@@ -122,7 +123,7 @@ class LezWatch_Shortcodes{
 	 * @since 1.0
 	*/
 	public function google_ads( $atts ) {
-	
+
 		$ads = '
 			<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 			<!-- Responsive -->
@@ -135,9 +136,56 @@ class LezWatch_Shortcodes{
 			(adsbygoogle = window.adsbygoogle || []).push({});
 			</script>
 		';
-	
+
 		return $ads;
-	}	
+	}
+
+	/*
+	 * Display Author Box
+	 *
+	 * Usage: [author-box users=username]
+	 *
+	 * @since 1.0
+	*/
+	public function author_box( $atts ) {
+
+		if ( $atts['users'] == '' ) return;
+
+		wp_enqueue_style( 'author-box-shortcode', '/wp-content/mu-plugins/css/author-box.css' );
+
+		$users = explode(',', $atts['users'] );
+		$user_count = count( $users );
+
+		$columns = 'one-half';
+		if ( $user_count == 1 ) $columns = '';
+		if ( $user_count % 3 == 0 ) $columns = 'one-third';
+
+		$author_box = '<div class="author-box-shortcode">';
+
+		foreach( $users as $user ) {
+			$user = username_exists( sanitize_user( $user ) );
+			if ( $user ) {
+				$authordata    = get_userdata( $user );
+				$gravatar_size = 'genesis_author_box_gravatar_size' ;
+				$gravatar      = get_avatar( get_the_author_meta( 'email', $user ), $gravatar_size );
+				$description   = wpautop( get_the_author_meta( 'description', $user ) );
+				$username      = get_the_author_meta( 'display_name' , $user );
+
+				$author_box   .= '
+					<section class="author-box '. $columns .'">'
+					. $gravatar
+					. '<h4 class="author-box-title"><span itemprop="name">' . $username . '</span></h4>
+					<div class="author-box-content" itemprop="description">'. $description .'</div>
+					</section>
+				';
+			}
+		}
+
+		$author_box .= '</div>';
+
+		return $author_box;
+	}
+
 }
 
 new LezWatch_Shortcodes();
