b538af1a774cf61f9a4253a485a038db020a60b6
Mika Ipstenu Epstein <ipstenu@ipstenu.org>
2017-05-07 10:02:59 -0700

    Formatting

--- per-site.php
+++ per-site.php
@@ -30,7 +30,7 @@ class LP_Per_Site {
 
 	function lezpress() {
 		add_filter( 'genesis_footer_creds_text', function( $creds ) {
-		    $creds = 'Copyright [footer_copyright first="2016"] <a href="https://lezpress.com">Lez Press</a> &middot; <a href="https://lezpress.com/terms-of-use/">Terms of Use</a> &middot; Powered by the <a href="http://www.shareasale.com/r.cfm?b=830048&u=728549&m=28169&urllink=&afftrack=">Showcase Pro Theme</a> on the <a href="http://www.shareasale.com/r.cfm?b=346198&u=728549&m=28169&urllink=&afftrack=">Genesis Framework</a>, [footer_wordpress_link], and <a href="//liquidweb.evyy.net/c/294289/297656/4464">Liquidweb Hosting</a><img height="0" width="0" src="//liquidweb.evyy.net/i/294289/297656/4464" style="position:absolute;visibility:hidden;" border="0" /> &middot; [footer_loginout]';
+		    $creds = 'Copyright [footer_copyright first="2016"] <a href="https://lezpress.com">Lez Press</a> &middot; <a href="https://lezpress.com/terms-of-use/">Terms of Use</a> <br /> Powered by the <a href="http://www.shareasale.com/r.cfm?b=830048&u=728549&m=28169&urllink=&afftrack=">Showcase Pro Theme</a> on the <a href="http://www.shareasale.com/r.cfm?b=346198&u=728549&m=28169&urllink=&afftrack=">Genesis Framework</a>, [footer_wordpress_link], and <a href="//liquidweb.evyy.net/c/294289/297656/4464">Liquidweb Hosting</a><img height="0" width="0" src="//liquidweb.evyy.net/i/294289/297656/4464" style="position:absolute;visibility:hidden;" border="0" /> <br /> [footer_loginout]';
 		    return $creds;
 		}, 10, 2 );
 	}
