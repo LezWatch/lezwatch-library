e556e1c4320544eb4e0abba0a1faef0103b0c7a3
Mika Ipstenu Epstein <ipstenu@ipstenu.org>
2017-09-07 19:15:25 -0700

    Add rainbow to the wp menu bar

--- functions.php
+++ functions.php
@@ -32,6 +32,7 @@ class LezPress_Network {
 	function __construct() {
 		self::$version = '2.0.0';
 		add_action( 'wp_enqueue_scripts', array( $this,  'wp_enqueue_scripts' ) );
+		add_action( 'wp_before_admin_bar_render', array( $this, 'rainbow_bar' ) );
 	}
 
 	/**
@@ -40,6 +41,46 @@ class LezPress_Network {
 	function wp_enqueue_scripts() {
 		// Cat Signal
 		wp_enqueue_script( 'cat-signal', content_url() . '/library/assets/js/catsignal.js', array(), self::$version, true );
+	}
+
+	/**
+	 * GAY
+	 */
+	function rainbow_bar() {
+	?>
+		<style type="text/css">
+			#wpadminbar {
+				background: linear-gradient(
+					to bottom,
+					#e24c3e 0%,
+					#e24c3e 16.66667%,
+					#f47d3b 16.66667%,
+					#f47d3b 33.33333%,
+					#fdb813 33.33333%,
+					#fdb813 50%,
+					#74bb5d 50%,
+					#74bb5d 66.66667%,
+					#38a6d7 66.66667%,
+					#38a6d7 83.33333%,
+					#8c7ab8 83.33333%,
+					#8c7ab8 100% );
+			}
+			#wpadminbar,
+			#wpadminbar .quicklinks > ul > li {
+				-webkit-box-shadow: unset;
+				-moz-box-shadow: unset;
+				box-shadow: unset;
+			}
+			#wpadminbar .ab-top-menu > li > a {
+				/* background-color: rgba( 50, 55, 60, .85 ); */
+			}
+			
+			#wpadminbar .ab-item, #wpadminbar a.ab-item, #wpadminbar > #wp-toolbar span.ab-label, #wpadminbar > #wp-toolbar span.noticon,
+			#wpadminbar .ab-icon, #wpadminbar .ab-icon:before, #wpadminbar .ab-item:before, #wpadminbar .ab-item:after {
+				color: #000;
+			}
+		</style>
+	<?php
 	}
 
 }
